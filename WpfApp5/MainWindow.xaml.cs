bd68f8685edf734d36aaedc8889ed6f49420eed9
Marat <maratf76@gmail.com>
2022-02-10 10:15:46 +0500

    Добавьте файлы проекта.

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,223 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public class MyMathModel : Prism.Mvvm.BindableBase
+    {
+
+
+
+        private readonly ObservableCollection<int> _myValues = new ObservableCollection<int>();
+        public readonly ReadOnlyObservableCollection<int> MyPublicValues;
+        public MyMathModel()
+        {
+            MyPublicValues = new ReadOnlyObservableCollection<int>(_myValues);
+        }
+        //добавление в коллекцию числа и уведомление об изменении суммы
+        public void AddValue(int value)
+        {
+            _myValues.Add(value);
+            RaisePropertyChanged("Sum");
+        }
+        //проверка на валидность, удаление из коллекции и уведомление об изменении суммы
+        public void RemoveValue(int index)
+        {
+            //проверка на валидность удаления из коллекции - обязанность модели
+            if (index >= 0 && index < _myValues.Count) _myValues.RemoveAt(index);
+            RaisePropertyChanged("Sum");
+        }
+        public int Sum => MyPublicValues.Sum(); //сумма
+    }
+
+
+    public class MainVM : BindableBase
+    {
+        private void GetColorId()
+        {
+            ColorCollection.Add(new ColorsServices(0, Colors.Black, nameof(Colors.Black)));
+            ColorCollection.Add(new ColorsServices(1, Colors.Yellow, nameof(Colors.Yellow)));
+            ColorCollection.Add(new ColorsServices(2, Colors.Green, nameof(Colors.Green)));
+            ColorCollection.Add(new ColorsServices(3, Colors.Gray, nameof(Colors.Gray)));
+            ColorCollection.Add(new ColorsServices(4, Colors.PaleGreen, nameof(Colors.PaleGreen)));
+            ColorCollection.Add(new ColorsServices(5, Colors.Violet, nameof(Colors.Violet)));
+            ColorCollection.Add(new ColorsServices(6, Colors.CadetBlue, nameof(Colors.CadetBlue)));
+        }
+        private ObservableCollection<ColorsServices> ColorCollection
+        { get; set; }
+
+        public ObservableCollection<ColorsServices> ColorList
+        {
+            get { return ColorCollection; }
+            set { ColorCollection = value; }
+        }
+
+        public ColorsServices selectedItem { get; set; }
+
+        readonly MyMathModel _model = new MyMathModel();
+        public MainVM()
+        {
+            ColorCollection = new ObservableCollection<ColorsServices>();
+            ColorList = new ObservableCollection<ColorsServices>();
+            GetColorId();
+
+            //таким нехитрым способом мы пробрасываем изменившиеся свойства модели во View
+            _model.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
+            AddCommand = new DelegateCommand<string>(str => {
+                //проверка на валидность ввода - обязанность VM
+                int ival;
+                if (int.TryParse(str, out ival)) _model.AddValue(ival);
+            });
+            RemoveCommand = new DelegateCommand<int?>(i => {
+                if (i.HasValue) _model.RemoveValue(i.Value);
+            });
+        }
+        public DelegateCommand<string> AddCommand { get; }
+        public DelegateCommand<int?> RemoveCommand { get; }
+        public int Sum => _model.Sum;
+        public ReadOnlyObservableCollection<int> MyValues => _model.MyPublicValues;
+    }
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    public class ColorsServices : BaseViewModel
+    {
+        public ColorsServices(int id, Color name, string textName)
+        {
+            Id = id;
+            Name = name;
+            TextName = textName;
+        }
+        private int id;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged( nameof(Id));
+            }
+        }
+
+        private Color name;
+        public Color Name
+
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string textName;
+        public string TextName
+        {
+            get
+            {
+                return textName;
+            }
+            set
+            {
+                textName = value;
+                OnPropertyChanged(nameof(TextName));
+            }
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainVM ssmain;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ssmain = new MainVM();
+            DataContext = ssmain;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w = new Window1();
+            w.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Define parameters used to create the BitmapSource.
+            PixelFormat pf = PixelFormats.Bgr32;
+            int width = 200;
+            int height = 200;
+            int rawStride = (width * pf.BitsPerPixel + 7) / 8;
+            byte[] navBarRawImage = new byte[rawStride * height];
+
+            // Initialize the image with data.
+            /*Random value = new Random();
+            value.NextBytes(rawImage);*/
+
+            for (int y = 0; y < height; y++)
+                for(int x = 0; x < width; x++)
+                {
+                    navBarRawImage[y * width * 4 + 4 * x] = 0;//(byte)(( y) % 255);  blue
+                    navBarRawImage[y * width * 4 + 4 * x + 1] = 0;//(byte)((x * y) % 255);  green
+                    navBarRawImage[y * width * 4 + 4 * x + 2] = 0 ;//(byte)((x ) % 255);  red
+                    navBarRawImage[y * width*4 + 4 * x+3] = (byte)((x*y)%255);
+                }
+
+            // Create a BitmapSource.
+            BitmapSource bitmap = BitmapSource.Create(width, height,
+                96, 96, pf, null,
+                navBarRawImage, rawStride);
+
+            imager1.Source = bitmap;
+
+            // Create an image element;
+            /*Image myImage = new Image();
+            myImage.Width = 200;
+            // Set image source.
+            myImage.Source = bitmap;*/
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ColorsServices tt = ((WpfApp5.ColorsServices)((object[])e.AddedItems)[0]);
+            ColorsServices tt1 = (sender as ComboBox).SelectedItem as ColorsServices;
+            ColorsServices tt2 = ssmain.selectedItem;
+        }
+    }
+}
---
+++ WpfApp5/Window1.xaml.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    /// 
+
+    public class DrawingCanvas : Panel
+    {
+        private List<Visual> visuals = new List<Visual>();
+
+        protected override Visual GetVisualChild(int index) => visuals[index];
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                return visuals.Count;
+            }
+        }
+
+        public void AddVisual(Visual visual)
+        {
+            visuals.Add(visual);
+
+            base.AddVisualChild(visual);
+            base.AddLogicalChild(visual);
+        }
+
+        public void DeleteVisual(Visual visual)
+        {
+            visuals.Remove(visual);
+
+            base.RemoveVisualChild(visual);
+            base.RemoveLogicalChild(visual);
+        }
+
+        public DrawingVisual GetVisual(Point point)
+        {
+            HitTestResult hitResult = VisualTreeHelper.HitTest(this, point);
+            return hitResult.VisualHit as DrawingVisual;
+        }
+
+        private List<Visual> hits = new List<Visual>();
+        public List<Visual> GetVisuals(Geometry region)
+        {
+            hits.Clear();
+            GeometryHitTestParameters parameters = new GeometryHitTestParameters(region);
+            HitTestResultCallback callback = new HitTestResultCallback(this.HitTestCallback);
+            VisualTreeHelper.HitTest(this, null, callback, parameters);
+            return hits;
+        }
+
+        private HitTestResultBehavior HitTestCallback(HitTestResult result)
+        {
+            GeometryHitTestResult geometryResult = (GeometryHitTestResult)result;
+            DrawingVisual visual = result.VisualHit as DrawingVisual;
+            if (visual != null &&
+                geometryResult.IntersectionDetail == IntersectionDetail.FullyInside)
+            {
+                hits.Add(visual);
+            }
+            return HitTestResultBehavior.Continue;
+        }
+
+    }
+
+    public partial class Window1 : Window
+    {
+        public class VisualHost : UIElement
+        {
+            public Visual Visual { get; set; }
+
+            protected override int VisualChildrenCount
+            {
+                get { return Visual != null ? 1 : 0; }
+            }
+
+            protected override Visual GetVisualChild(int index)
+            {
+                return Visual;
+            }
+        }
+
+        public Window1()
+        {
+            InitializeComponent();
+
+            this.Activated += Window1_Activated;
+
+            DrawTest();
+        }
+
+        private void Window1_Activated(object sender, EventArgs e)
+        {
+
+            DrawAxis();
+            //throw new NotImplementedException();
+        }
+
+        void DrawAxis()
+        {
+            double startx = 20;
+            double endy = 20;
+
+            double h = Math.Round( grid1.ActualHeight-20);
+            double w = Math.Round( grid1.ActualWidth-10-endy);
+
+            grid1.Children.Clear();
+
+            Path p = new Path();
+            string myPathGeom = "M " + startx.ToString() + "," + h.ToString()+ " L " + startx.ToString() + "," + endy.ToString() + " M " + startx.ToString() + "," + h.ToString() + " L " + w.ToString() + "," + h.ToString() ;
+
+            int vertRisk = (int)Math.Round(h / 10);
+            int horizRisk = (int)Math.Round(w  / 10);
+
+            for (byte i = 1; i < (horizRisk-1); i++)
+            {
+                myPathGeom += " M ";
+                myPathGeom += (startx+i * 10).ToString() + ","+h.ToString();
+                myPathGeom += " L ";
+                myPathGeom += (startx+i * 10).ToString() + "," + (h-3).ToString();
+            }
+
+            for (byte i = 1; i < (vertRisk-2); i++)
+            {
+                myPathGeom += " M ";
+                myPathGeom += "" + startx.ToString() + "," + (h-i * 10).ToString();
+                myPathGeom += " L ";
+                myPathGeom += "" + (startx+3).ToString() + "," + (h-i * 10).ToString();
+            }
+
+         //   myPathGeom += " M 5,15 L 10,10 L 15,15";
+          //  myPathGeom += " M " + (w-5).ToString() + "," + (h-5).ToString() + " L " + w.ToString() + "," + h.ToString() + " L " + (w - 5).ToString() + "," + (h+5).ToString() ;
+            myPathGeom += " M 0,0 Z";
+            p.Data = Geometry.Parse(myPathGeom);
+            p.StrokeThickness = 2;
+            p.Stroke = Brushes.Black;
+
+            grid1.Children.Add(p);
+
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DrawAxis();
+        }
+
+        void DrawTest()
+        {
+            drawTest.Children.Clear();
+
+            DrawingVisual drawingVisual = new DrawingVisual();
+
+            // Retrieve the DrawingContext in order to create new drawing content.
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+            // Create a rectangle and draw it in the DrawingContext.
+            Rect rect = new Rect(new System.Windows.Point(0, 0), new System.Windows.Size(100, 100));
+            drawingContext.DrawRectangle(System.Windows.Media.Brushes.Aqua, (System.Windows.Media.Pen)null, rect);
+
+            // Persist the drawing content.
+            drawingContext.Close();
+            drawTest.Children.Add(new VisualHost { Visual = drawingVisual });
+
+            DrawingCanvas cnvDraw = new DrawingCanvas();
+
+            testDrawVisual.Children.Clear();
+
+            cnvDraw.Children.Clear();
+
+            Point point2 = new Point(10, 80);
+            Point point3 = new Point(50, 50);
+            StreamGeometry streamGeometry = new StreamGeometry();
+            using (StreamGeometryContext geometryContext = streamGeometry.Open())
+            {
+                geometryContext.BeginFigure(new Point(1, 50), false, false);
+                PointCollection points = new PointCollection
+                                             {
+                                                 point2,
+                                                 point3
+                                             };
+                geometryContext.PolyLineTo(points, true, true);
+            }
+
+            DrawingVisual visual = new DrawingVisual();
+            using (DrawingContext dc = visual.RenderOpen())
+            {
+              //  Pen drawingpen = new Pen(Brushes.Black, 3);
+              //  dc.DrawLine(drawingpen, new Point(0, 50), new Point(50, 0));
+              //  dc.DrawLine(drawingpen, new Point(50, 0), new Point(100, 50));
+              //  dc.DrawLine(drawingpen, new Point(0, 50), new Point(100, 50));
+
+
+
+
+                    dc.DrawGeometry(Brushes.LightGreen, new Pen(Brushes.Aqua, 2), streamGeometry);
+
+            }
+
+            cnvDraw.AddVisual(visual);
+
+            testDrawVisual.Children.Add(cnvDraw);
+
+        }
+
+
+
+    }
+}
